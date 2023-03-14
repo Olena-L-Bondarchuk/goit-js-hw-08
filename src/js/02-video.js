dd611ea834064c5a6bbe1caf7c761e3e67ac804c
Olena-L-Bondarchuk <olena.l.bondarchuk@gmail.com>
2023-03-15 00:43:29 +0100

    add task 2 y task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const PLAYER_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(event) {
+  localStorage.setItem(PLAYER_TIME, JSON.stringify(event.seconds));
+}
+
+const currentTime = JSON.parse(localStorage.getItem(PLAYER_TIME));
+player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+const USER_DATA = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', resetForm);
+
+function onFormInput(event) {
+  const obj = {
+    email: input.value,
+    message: textarea.value,
+  };
+  localStorage.setItem(USER_DATA, JSON.stringify(obj));
+}
+
+function onFormUpdate() {
+  const getData = JSON.parse(localStorage.getItem(USER_DATA));
+  if (getData) {
+    input.value = getData.email;
+    textarea.value = getData.message;
+  }
+}
+onFormUpdate();
+
+function resetForm(event) {
+  event.preventDefault();
+  console.log(localStorage.getItem(USER_DATA));
+  localStorage.removeItem(USER_DATA);
+  event.currentTarget.reset();
+}
